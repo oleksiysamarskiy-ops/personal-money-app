299fb80fcd1826a7b5173a89f39325744fc76995
oleksiysamarskiy-ops <oleksiy.samarskiy@gmail.com>
2026-06-10 12:06:07 +0200

    Delete src directory

--- src/store/userContext.tsx
+++
@@ -1,9 +0,0 @@
-import React, { createContext, useContext } from 'react'
-
-const UserIdContext = createContext<string>('')
-
-export const UserIdProvider = ({ userId, children }: { userId: string; children: React.ReactNode }) => (
-  <UserIdContext.Provider value={userId}>{children}</UserIdContext.Provider>
-)
-
-export const useUserId = () => useContext(UserIdContext)
